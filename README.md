4a5a762b7287726d9ed45440813218c9eff6ca5f
Bilal Niaz <createlogic@gmail.com>
2015-03-05 16:28:05 +0400

    Plugin files added

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # InlineSVG
 This is a jQuery plugin that takes an image selector as an argument having a SVG as source. Then it inlines the SVG so that the SVG stroke and path can be manipulated using plain CSS.
+
+
+## Usage ##
+
+### HTML ###
+```<img class="svg" src="http://addc.digitaslbiacademy.com/img/icon/svg/00-login.svg" alt="" />```
+
+
+### CSS ###
+```
+#!CSS
+.svg path stroke {
+	color:#00ff88;
+}
+```
+
+### JS ###
+```$('.svg').inlineSVG();```
---
+++ jquery.inlinesvg.js
@@ -0,0 +1,43 @@
+/*!
+ * jquery.inlinesvg
+ * https://github.com/createlogic/InlineSVG
+ * Copyright (c) 2010 - 2015 Bilal Niaz Awan
+ * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
+ * Version: 1.0.0
+ */
+
+(function($){
+    $.fn.inlineSVG = function(){
+
+        return this.each(function(){
+
+            var $img = jQuery(this);
+            var imgID = $img.attr('id');
+            var imgClass = $img.attr('class');
+            var imgURL = $img.attr('src');
+
+            jQuery.get(imgURL, function(data) {
+                // Get the SVG tag, ignore the rest
+                var $svg = jQuery(data).find('svg');
+
+                // Add replaced image's ID to the new SVG
+                if(typeof imgID !== 'undefined') {
+                    $svg = $svg.attr('id', imgID);
+                }
+                // Add replaced image's classes to the new SVG
+                if(typeof imgClass !== 'undefined') {
+                    $svg = $svg.attr('class', imgClass+' replaced-svg');
+                }
+
+                // Remove any invalid XML tags as per http://validator.w3.org
+                $svg = $svg.removeAttr('xmlns:a');
+
+                // Replace image with new SVG
+                $img.replaceWith($svg);
+
+            }, 'xml');
+
+
+        });
+    };
+})(jQuery);
