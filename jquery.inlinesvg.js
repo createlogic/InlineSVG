c45c14ab9ca4bf04b3a92717fac477b708ac624c
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2015-05-14 01:35:23 +0300

    fixed description in comment

--- jquery.inlinesvg.js
+++ jquery.inlinesvg.js
@@ -1,5 +1,9 @@
 /*!
- * jquery.inlinesvg
+ * InlineSVG
+ *
+ * This is a jQuery plugin that takes an image selector as an argument having a
+ * SVG as source. Then it inlines the SVG so that the SVG stroke and path can
+ * be manipulated using plain CSS.
  *
  * @license MIT
  * @version 1.1.0
